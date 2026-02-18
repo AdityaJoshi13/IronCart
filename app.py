db8a5cad069def57b8b0d523a8ffe2db2ace440d
Aditya Joshi <adityajoshi@Adityas-MacBook-Air.local>
2026-02-18 13:23:13 +0530

    Changed files code

--- app.py
+++ app.py
@@ -168,5 +168,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         seed_database()
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
